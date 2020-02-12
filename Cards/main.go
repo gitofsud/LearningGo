e727170754bcfc9f6a2c81e34abcdc1102f80362
gitofsud <accessthecrash@gmail.com>
2020-02-12 23:57:45 +0530

    Type Declarations and Receivers

--- Cards/main.go
+++ Cards/main.go
@@ -1,22 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	// Variable Declarations
-	// var card string = "Ace of Spades"
-	// card := "Ace of Spades"
-	// card = "Five of Diamonds"
-
-	// Function Declaration
-	// card := newCard()
-
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
