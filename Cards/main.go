1d7bff93ff67e6d1a48c2bd721aba666d38220a0
gitofsud <accessthecrash@gmail.com>
2020-02-12 20:37:55 +0530

    Variable Declarations

---
+++ Cards/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variable Declarations
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
