02e5e1485f2198271939744d05cbff4a5c3544f4
gitofsud <accessthecrash@gmail.com>
2020-02-12 23:27:36 +0530

    Function Declarations

--- Cards/main.go
+++ Cards/main.go
@@ -5,8 +5,15 @@ import "fmt"
 func main() {
 	// Variable Declarations
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	// card := "Ace of Spades"
+	// card = "Five of Diamonds"
+
+	// Function Declaration
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
