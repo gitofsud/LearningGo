0f848bf10a4251d30984f21db5e7ecfe8ed1db3f
gitofsud <accessthecrash@gmail.com>
2020-02-13 11:26:10 +0530

    Slice Range and Multiple Returns

--- Cards/main.go
+++ Cards/main.go
@@ -1,10 +1,12 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
 }
 
 func newCard() string {
